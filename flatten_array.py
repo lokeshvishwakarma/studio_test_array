8b694bdd10137dc332527c560525c61f15781549
Lokesh Vishwakarma <lokesh@cueclad.com>
2019-02-07 00:29:02 +0530

    flatten method tested successfully

--- flatten_array.py
+++ flatten_array.py
@@ -1,15 +1,20 @@
-def flatten(nested_arr):
+def flatten(element, flat_arr=None):
     """
     This method takes nested array as a parameter and
     returns as flattened version of it.
-    :nested_arr : list
-    example:    nested_arr=[1,[2,3,[4],5,[6]]]
+    :element : list
+    example:    element=[1,[2,3,[4],5,[6]]]
                 output = [1,2,3,4,5,6]
     """
-    flat_arr = []
-    for item in nested_arr:
-        if type(item) == type(list()):
-            flat_arr.extend(flatten(item))
-        else:
-            flat_arr.append(item)
+    if flat_arr == None:
+        flat_arr = []
+    
+    if not isinstance(element, list):
+        flat_arr.append(element)
+    else:
+        for item in element:
+            flatten(item, flat_arr)
     return flat_arr
+
+in_list = [1,[2,3,[4],5,[6]]]
+print(flatten(in_list))
---
+++ test_flatten_mod.py
@@ -0,0 +1,21 @@
+import unittest
+import flatten_array
+
+
+class TestFlatten(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(flatten_array.flatten([]), [])
+
+    def test_flatten(self):
+        flat_list = flatten_array.flatten([1, [2, 3, [4], 5, [6]]])
+        for item in flat_list:
+            if isinstance(item, list):
+                flag = False
+            else:
+                flag = True
+
+        self.assertEqual(flag, True)
+
+    def test_diff_dataset(self):
+        self.assertEqual(flatten_array.flatten(
+            [2, 'loki', [3, 10]]), [2, 'loki', 3, 10])
